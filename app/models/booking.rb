852fe08089ac85ad1282748186c6734071bc05e8
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-04-21 23:09:37 +0200

    ajout des associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ApplicationRecord
   TRAVELLERS = [1, 2, 3, 4]
+  belongs_to :flat
+  belongs_to :client
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,6 +1,8 @@
 class Flat < ApplicationRecord
   has_one_attached :picture
   has_many_attached :photos
+  has_many :bookings
+  belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
