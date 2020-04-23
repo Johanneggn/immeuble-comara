e1dd8886ff90e368efd4072171c0a5eae946a34b
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-04-23 22:41:18 +0200

    correction migration

--- db/migrate/20200423202637_add_reference_to_flat.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToFlat < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :flats, :user, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,20 @@
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_04_23_202637) do
+=======
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_20_094402) do
+>>>>>>> parent of 3ab54b2... changement migration
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,9 +101,9 @@ ActiveRecord::Schema.define(version: 2020_04_23_202637) do
     t.float "latitude"
     t.string "address"
     t.string "capacity"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
