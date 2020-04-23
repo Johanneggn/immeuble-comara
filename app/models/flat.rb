f3678c160b7369b188204962d232b30a34cc3bdd
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-04-23 22:56:33 +0200

    test association flat / user

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,7 +3,7 @@ class Flat < ApplicationRecord
   has_many_attached :photos
   has_many :bookings
   has_many :equipments
-  belongs_to :user
+  # belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
