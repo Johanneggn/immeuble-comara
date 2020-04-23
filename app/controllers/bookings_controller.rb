7f4abbec9f1afb92d35295222a44093c9daf2588
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-04-23 22:10:11 +0200

    changement des images dans les cards de la home

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,5 +7,16 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @client = Client.new(client_params)
+    @booking = Booking.new(booking_params)
+    @booking.client = @client
+    @booking.flat = current_flat
+    @booking.total_price = @booking.flat.price_per_day*((@booking.end_date - @booking.start_date).to_i)
+    @booking.status = 'pending'
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
 end
