4b1de1455137fa8e4ecc346ed9b409859ca365ca
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-04-24 00:11:45 +0200

    correction associations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,9 +1,9 @@
 class Flat < ApplicationRecord
+  belongs_to :user
   has_one_attached :picture
   has_many_attached :photos
   has_many :bookings
   has_many :equipments
-  # belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
