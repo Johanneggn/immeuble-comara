744ef5c4e054d2cf964f3f9750fc0afae701cd1c
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-04-24 21:32:14 +0200

    changement du système de capacité pour les appartements

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,16 @@
-ActiveRecord::Schema.define(version: 2020_04_20_094402) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_04_24_094837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_094402) do
     t.bigint "client_id"
     t.bigint "flat_id"
     t.integer "travellers"
+    t.string "status"
     t.index ["client_id"], name: "index_bookings_on_client_id"
     t.index ["flat_id"], name: "index_bookings_on_flat_id"
   end
@@ -50,31 +62,12 @@ ActiveRecord::Schema.define(version: 2020_04_20_094402) do
   end
 
   create_table "equipment", force: :cascade do |t|
-    t.boolean "kitchen"
-    t.boolean "air_conditioner"
-    t.boolean "hangers"
-    t.boolean "washing_machine"
-    t.boolean "television"
-    t.boolean "private_entrance"
-    t.boolean "sheets"
-    t.boolean "work_space"
-    t.boolean "fridge"
-    t.boolean "dishes"
-    t.boolean "utensil"
-    t.boolean "cooker"
-    t.boolean "balcony"
-    t.boolean "backyard"
-    t.boolean "carbon_monoxide_detector"
-    t.boolean "hot_water"
-    t.boolean "parking"
-    t.boolean "long_stay"
-    t.boolean "tumble_dryer"
-    t.boolean "wi_fi"
-    t.boolean "store"
-    t.boolean "luggage"
     t.bigint "flat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.boolean "available"
+    t.boolean "pricing"
     t.index ["flat_id"], name: "index_equipment_on_flat_id"
   end
 
@@ -85,10 +78,11 @@ ActiveRecord::Schema.define(version: 2020_04_20_094402) do
     t.float "longitude"
     t.float "latitude"
     t.string "address"
-    t.string "capacity"
-    t.bigint "user_id"
+    t.string "max_capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.integer "min_capacity"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ puts "Deleting all bookings..."
 Booking.destroy_all
 puts "Deleting all clients..."
 Client.destroy_all
+puts "Deleting all equipments"
+Equipment.destroy_all
 puts "Deleting all flats..."
 Flat.destroy_all
 puts "Done deleting all flats"
@@ -40,7 +42,8 @@ studio = Flat.new(
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   price_per_day: 30,
   address: "Abidjan",
-  capacity: 2)
+  min_capacity: 1,
+  max_capacity: 2)
 studio.photos.attach(io: studio_photo_1, filename: "photo_3.jpg", content_type: 'image/jpg')
 studio.photos.attach(io: studio_photo_2, filename: "photo_2.jpg", content_type: 'image/jpg')
 studio.photos.attach(io: studio_photo_3, filename: "photo_1.jpg", content_type: 'image/jpg')
@@ -69,7 +72,8 @@ studio_terrasse = Flat.new(
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   price_per_day: 25,
   address: "Abidjan",
-  capacity: 2)
+  min_capacity: 1,
+  max_capacity: 2)
 studio_terrasse.photos.attach(io: studio_terrasse_photo_1, filename: "photo_1.jpg", content_type: 'image/jpg')
 studio_terrasse.photos.attach(io: studio_terrasse_photo_2, filename: "photo_2.jpg", content_type: 'image/jpg')
 studio_terrasse.picture.attach(io: studio_terrasse_photo_3, filename: "photo_3.jpg", content_type: 'image/jpg')
@@ -98,7 +102,8 @@ studio_balcon = Flat.new(
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   price_per_day: 40,
   address: "Abidjan",
-  capacity: 4)
+  min_capacity: 3,
+  max_capacity: 4)
 studio_balcon.photos.attach(io: studio_balcon_photo_1, filename: "photo_1.jpg", content_type: 'image/jpg')
 studio_balcon.photos.attach(io: studio_balcon_photo_2, filename: "photo_2.jpg", content_type: 'image/jpg')
 studio_balcon.photos.attach(io: studio_balcon_photo_3, filename: "photo_3.jpg", content_type: 'image/jpg')
@@ -127,7 +132,8 @@ appartement_chambre = Flat.new(
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   price_per_day: 25,
   address: "Abidjan",
-  capacity: 4)
+  min_capacity: 3,
+  max_capacity: 4)
 appartement_chambre.photos.attach(io: appartement_chambre_image_1, filename: "photo_1.jpg", content_type: 'image/jpg')
 appartement_chambre.photos.attach(io: appartement_chambre_image_2, filename: "photo_2.jpg", content_type: 'image/jpg')
 appartement_chambre.photos.attach(io: appartement_chambre_image_3, filename: "photo_3.jpg", content_type: 'image/jpg')
@@ -157,7 +163,8 @@ appartement_chambres = Flat.new(
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   price_per_day: 25,
   address: "Abidjan",
-  capacity: 4)
+  min_capacity: 3,
+  max_capacity: 4)
 appartement_chambres.photos.attach(io: appartement_chambres_image_1, filename: "photo_1.jpg", content_type: 'image/jpg')
 appartement_chambres.photos.attach(io: appartement_chambres_image_2, filename: "photo_2.jpg", content_type: 'image/jpg')
 appartement_chambres.photos.attach(io: appartement_chambres_image_3, filename: "photo_3.jpg", content_type: 'image/jpg')
@@ -186,7 +193,8 @@ appart_chambres = Flat.new(
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   price_per_day: 25,
   address: "Abidjan",
-  capacity: 2)
+  min_capacity: 1,
+  max_capacity: 2)
 appart_chambres.photos.attach(io: appart_chambres_image_1, filename: "photo_1.jpg", content_type: 'image/jpg')
 appart_chambres.photos.attach(io: appart_chambres_image_2, filename: "photo_2.jpg", content_type: 'image/jpg')
 appart_chambres.photos.attach(io: appart_chambres_image_3, filename: "photo_3.jpg", content_type: 'image/jpg')
@@ -201,6 +209,181 @@ appart_chambres.user = johanne
 appart_chambres.save!
 
 puts "Done seeding flats"
+
+puts "Seeding equipments"
+
+cuisine = Equipment.new(name: 'kitchen', available: true, pricing: false )
+cuisine.flat = studio
+# cuisine.flat = studio_terrasse
+# cuisine.flat = studio_balcon
+# cuisine.flat = appartement_chambre
+# cuisine.flat = appartement_chambres
+# cuisine.flat = appart_chambres
+cuisine.save!
+
+climatisation = Equipment.new(name: 'air_conditioner', available: true, pricing: false )
+climatisation.flat = studio
+# climatisation.flat = studio_terrasse
+# climatisation.flat = studio_balcon
+# climatisation.flat = appartement_chambre
+# climatisation.flat = appartement_chambres
+# climatisation.flat = appart_chambres
+climatisation.save!
+
+
+cintre = Equipment.new(name: 'hangers', available: true, pricing: false )
+cintre.flat = studio
+# cintre.flat = studio_terrasse
+# cintre.flat = studio_balcon
+# cintre.flat = appartement_chambre
+# cintre.flat = appartement_chambres
+# cintre.flat = appart_chambres
+cintre.save!
+
+machine_a_laver = Equipment.new(name: 'washing_machine', available: true, pricing: false )
+machine_a_laver.flat = studio
+# machine_a_laver.flat = studio_terrasse
+# machine_a_laver.flat = studio_balcon
+# machine_a_laver.flat = appartement_chambre
+# machine_a_laver.flat = appartement_chambres
+# machine_a_laver.flat = appart_chambres
+machine_a_laver.save!
+
+television = Equipment.new(name: 'television', available: false, pricing: false )
+television.flat = studio
+# television.flat = studio_terrasse
+# television.flat = studio_balcon
+# television.flat = appartement_chambre
+# television.flat = appartement_chambres
+television.save!
+
+entree_privee = Equipment.new(name: 'private_entrance', available: true, pricing: false )
+entree_privee.flat = studio
+# entree_privee.flat = studio_terrasse
+# entree_privee.flat = studio_balcon
+# entree_privee.flat = appartement_chambre
+# entree_privee.flat = appartement_chambres
+# entree_privee.flat = appart_chambres
+entree_privee.save!
+
+draps = Equipment.new(name: 'sheets', available: true, pricing: false )
+draps.flat = studio
+# draps.flat = studio_terrasse
+# draps.flat = studio_balcon
+# draps.flat = appartement_chambre
+# draps.flat = appartement_chambres
+# draps.flat = appart_chambres
+draps.save!
+
+espace_de_travail = Equipment.new(name: 'work_space', available: true, pricing: false )
+espace_de_travail.flat = studio
+# espace_de_travail.flat = studio_balcon
+# espace_de_travail.flat = appartement_chambre
+espace_de_travail.save!
+
+frigo = Equipment.new(name: 'fridge', available: true, pricing: false )
+frigo.flat = studio
+# frigo.flat = studio_terrasse
+# frigo.flat = studio_balcon
+# frigo.flat = appartement_chambre
+# frigo.flat = appartement_chambres
+# frigo.flat = appart_chambres
+frigo.save!
+
+vaisselle = Equipment.new(name: 'dishes', available: true, pricing: false )
+vaisselle.flat = studio
+# vaisselle.flat = studio_terrasse
+# vaisselle.flat = studio_balcon
+# vaisselle.flat = appartement_chambre
+# vaisselle.flat = appartement_chambres
+# vaisselle.flat = appart_chambres
+vaisselle.save!
+
+ustensil = Equipment.new(name: 'utensil', available: true, pricing: false )
+ustensil.flat = studio
+# ustensil.flat = studio_terrasse
+# ustensil.flat = studio_balcon
+# ustensil.flat = appartement_chambre
+# ustensil.flat = appartement_chambres
+# ustensil.flat = appart_chambres
+ustensil.save!
+
+# gaziniere = Equipment.new(name: 'cooker', available: true, pricing: false )
+# gaziniere.flat = studio_balcon
+# gaziniere.flat = appartement_chambre
+# gaziniere.flat = appartement_chambres
+# gaziniere.save!
+
+balcon = Equipment.new(name: 'balcony', available: true, pricing: false )
+balcon.flat = studio
+# balcon.flat = studio_terrasse
+# balcon.flat = appartement_chambres
+balcon.save!
+
+# arriere_cour = Equipment.new(name: 'backyard', available: true, pricing: false )
+# arriere_cour.flat = studio_balcon
+# arriere_cour.flat = appartement_chambre
+# arriere_cour.flat = appart_chambres
+# arriere_cour.save!
+
+detecteur = Equipment.new(name: 'carbon_monoxide_detector', available: true, pricing: false )
+detecteur.flat = studio
+# detecteur.flat = studio_balcon
+# detecteur.flat = appartement_chambre
+detecteur.save!
+
+eau_chaude = Equipment.new(name: 'hot_water', available: true, pricing: false )
+eau_chaude.flat = studio
+# eau_chaude.flat = studio_terrasse
+# eau_chaude.flat = studio_balcon
+# eau_chaude.flat = appartement_chambre
+# eau_chaude.flat = appartement_chambres
+# eau_chaude.flat = appart_chambres
+eau_chaude.save!
+
+parking = Equipment.new(name: 'parking', available: true, pricing: false )
+parking.flat = studio
+# parking.flat = studio_balcon
+# parking.flat = appartement_chambre
+parking.save!
+
+sejour_long = Equipment.new(name: 'long_stay', available: true, pricing: false )
+sejour_long.flat = studio
+# sejour_long.flat = studio_terrasse
+# sejour_long.flat = studio_balcon
+# sejour_long.flat = appartement_chambres
+sejour_long.save!
+
+# seche_linge = Equipment.new(name: 'tumble_dryer', available: true, pricing: false )
+# seche_linge.flat = studio_terrasse
+# seche_linge.flat = studio_balcon
+# seche_linge.flat = appartement_chambre
+# seche_linge.save!
+
+# wi_fi = Equipment.new(name: 'wi_fi', available: true, pricing: false )
+# wi_fi.flat = studio_terrasse
+# wi_fi.flat = studio_balcon
+# wi_fi.flat = appartement_chambres
+# wi_fi.save!
+
+store = Equipment.new(name: 'store', available: true, pricing: false )
+store.flat = studio
+# store.flat = studio_balcon
+# store.flat = appartement_chambres
+store.save!
+
+bagagerie = Equipment.new(name: 'luggage', available: true, pricing: false )
+bagagerie.flat = studio
+# bagagerie.flat = studio_terrasse
+# bagagerie.flat = studio_balcon
+# bagagerie.flat = appartement_chambre
+# bagagerie.flat = appartement_chambres
+# bagagerie.flat = appart_chambres
+bagagerie.save!
+
+
+puts "Done Seeding equipments"
+
 puts "seeding clients"
 # clients
 
@@ -209,32 +392,32 @@ puts "Done seeding clients"
 
 # bookings
 puts "Seeding bookings"
-booking_1 = Booking.new(total_price: 130, start_date: '30-06-2020', end_date: '05-07-2020')
+booking_1 = Booking.new(total_price: 130, start_date: '30-06-2020', end_date: '05-07-2020', status: "pending")
 booking_1.flat = studio
 booking_1.client = cedric
 booking_1.save! #2
 
-booking_2 = Booking.new(total_price: 130, start_date: '10-06-2020', end_date: '15-06-2020')
+booking_2 = Booking.new(total_price: 130, start_date: '10-06-2020', end_date: '15-06-2020', status: "confirmed")
 booking_2.flat = studio_terrasse
 booking_2.client = cedric
 booking_2.save! #2
 
-booking_3 = Booking.new(total_price: 130, start_date: '10-06-2020', end_date: '18-06-2020')
+booking_3 = Booking.new(total_price: 130, start_date: '10-06-2020', end_date: '18-06-2020', status: "denied")
 booking_3.flat = appartement_chambre
 booking_3.client = cedric
 booking_3.save! # 4
 
-booking_4 = Booking.new(total_price: 130, start_date: '30-06-2020', end_date: '05-07-2020')
+booking_4 = Booking.new(total_price: 130, start_date: '30-06-2020', end_date: '05-07-2020', status: "pending")
 booking_4.flat = appartement_chambres
 booking_4.client = cedric
 booking_4.save! # 4
 
-booking_5 = Booking.new(total_price: 130, start_date: '10-07-2020', end_date: '17-07-2020')
+booking_5 = Booking.new(total_price: 130, start_date: '10-07-2020', end_date: '17-07-2020', status: "pending")
 booking_5.flat = appart_chambres
 booking_5.client = cedric
 booking_5.save! # 2
 
-booking_6 = Booking.new(total_price: 130, start_date: '30-06-2020', end_date: '05-07-2020')
+booking_6 = Booking.new(total_price: 130, start_date: '30-06-2020', end_date: '05-07-2020', status: "confirmed")
 booking_6.flat = studio_balcon
 booking_6.client = cedric
 booking_6.save! # 4
