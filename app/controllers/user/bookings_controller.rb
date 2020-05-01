46e1b23b8c6ce2efaee22cacca44360c57921843
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-05-01 19:51:42 +0200

    correction prod cloudinary + ajout fonctionnalité

--- app/controllers/user/bookings_controller.rb
+++ app/controllers/user/bookings_controller.rb
@@ -1,10 +1,7 @@
 class User::BookingsController < ApplicationController
   def index
     @bookings = Booking.all
-    @dates = (Date.today..Date.today.next_year(5))
-
-    # calendar_day = (start_date..end_date)
-    # @begin_bookings = @bookings.group_by(&:start_date)
+    #calendar_day = (start_date..end_date)
     # @end_bookings = current_user.bookings.group_by(&:end_date)
     # @booking_in_progress = current_user.bookings.where('start_date > ? AND end_date < ?', calendar_day, calendar_day)
   end
