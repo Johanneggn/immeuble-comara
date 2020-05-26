6b3bfca4b18da9fa06324c5fd404cbf956d05e23
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-05-26 12:46:24 +0200

    début form booking user

--- app/controllers/user/bookings_controller.rb
+++ app/controllers/user/bookings_controller.rb
@@ -1,13 +1,28 @@
 class User::BookingsController < ApplicationController
   before_action :set_booking, only: %i[edit update destroy refused_booking accepted_booking]
+
   def index
     @bookings = Booking.all
     @bookings_accepted = Booking.where(status: 'confirmed')
     @clients = Client.all
   end
   def new
+    @booking = Booking.new
+    @display_flat_names = Flat.order(:name).map do | flat |
+      [flat.name, flat.id]
+    end
   end
+
   def create
+    @client = Client.new(client_params)
+    @booking = @client.booking
+    @booking.flat = Flat.find(params[:id])
+
+    if @booking.save
+      redirect_to user_bookings_path
+    else
+      render :new
+    end
   end
   def edit
   end
@@ -33,4 +48,25 @@ class User::BookingsController < ApplicationController
   def set_booking
     @booking = Booking.find(params[:id])
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :travellers)
+  end
+
+  def client_params
+    params.require(:client).permit(
+      :civilty,
+      :first_name,
+      :last_name,
+      :address,
+      :phone_number,
+      :email,
+      booking_attributes: [:start_date, :end_date, :travellers])
+  end
+
+  # def set_flat
+  #   @flat = Flat.all
+  # end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,6 @@
 class Booking < ApplicationRecord
   TRAVELLERS = [1, 2, 3, 4]
+  STATUS = ["pending", "refused", "confirmed"]
   belongs_to :flat
   belongs_to :client
   # accepts_nested_attributes_for :client
