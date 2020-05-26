59b0af77f14195167f5079e093053d38c36c86dd
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-05-26 11:32:22 +0200

    pages contact et presentation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :contact, :rgpd, :rules, :presentation]
 
   def home
     @flats = Flat.all
     @bookings = Booking.all
   end
+
+  def presentation; end
 end
